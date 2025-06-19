c6c10d3898790291597b20cf8675d4fb4ecbcdb6
AlexiaBojian1 <a.bojian@student.tue.nl>
2025-06-20 01:49:39 +0200

    httpserver

--- hackingapp/protocols/httpsserver.py
+++ hackingapp/protocols/httpsserver.py
@@ -2,5 +2,5 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='cert.pem', keyfile='key.pem', server_side=True)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='cert.pem', keyfile='key.pem', server_side=True, ssl_version=ssl.PROTOCOL_TLSv1)
 httpd.serve_forever()
